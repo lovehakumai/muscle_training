8b4ebb9a9c803f4fcc564cced67c12641c7161c3
lovehakumai <masaotomeza@gmail.com>
2026-08-30 17:23:30 +0900

    feat(pipeline) add efficient summary fact table

--- sql/task03/task03/models/mart/fct_monthly_merchant_financial_summary.sql
+++ sql/task03/task03/models/mart/fct_monthly_merchant_financial_summary.sql
@@ -1,9 +1,4 @@
-{{
-    config(
-        database = 'MUSCLE_DB_TASK03'
-    )
-}}
-with merchans as (
+with merchants as (
     select * from {{ ref('stg_apexpay__raw_merchants') }}
 )
 , terminals as (
@@ -13,4 +8,46 @@ with merchans as (
     select * from {{ ref('stg_apexpay__raw_transactions') }}
 )
 
-select * from transactions
+, agg_tran_by_merchant as (
+    select 
+        merchant_id
+        , count(*) as settled_tx_count 
+        , sum(amount) as total_settled_amount
+        , sum(fee_amount) as total_fee_amount
+    from 
+        transactions 
+    where
+        transaction_status = 'SETTLED'
+        and date_trunc(month, transaction_timestamp) = '2026-07-01'::timestamp
+    group by 
+        merchant_id
+)
+, agg_term_by_merchant as (
+    select
+        merchant_id
+        , count(distinct terminal_id) as active_terminal_count
+    from 
+        terminals
+    where 
+        valid_to is null 
+        and is_active = true 
+    group by 
+        merchant_id
+)
+, final as (
+    select
+        merchant_id
+        , merchant_name
+        , country_code
+        , total_settled_amount
+        , total_fee_amount
+        , settled_tx_count
+        , active_terminal_count
+    from merchants 
+    inner join agg_tran_by_merchant
+    using(merchant_id)
+    inner join agg_term_by_merchant
+    using(merchant_id)
+    
+)
+select * from final
