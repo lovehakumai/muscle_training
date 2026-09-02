f38f64bd96b5a262431506bd3c5a602222fa7e14
lovehakumai <masaotomeza@gmail.com>
2026-09-02 23:21:53 +0900

    feat(pipeline) add fillna to category value in arow with product_id 999, add validation in pd.merge that validates if the merge was m:1

--- python/task02/main.py
+++ python/task02/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # ----------------------------------------------------
 # Sales transaction data (Target to keep original row count)
@@ -24,9 +25,14 @@ def cleanse_raw_data(df_products:pd.DataFrame, df_sales:pd.DataFrame)-> pd.DataF
     """
     [CLENSE] df_products  
     Implements process for duplicated product_id, only product_id with max number of id will be remained.  
+    
     [MERGE] LEFT JOIN df_sales and df_products  
     key = [product_id, index]
-    [ASSERTION] df_sales's row number should not be changed  
+    Check if merge worked expectedly, many_to_one or one_to_one  
+    Implements process for the product_id 999, it'll be replaced to 'Unknown'.  
+
+    [ASSERTION] RAISE ASSERTION ERROR
+    df_sales's row number should not be changed  
     """
     # ----------------------------------------------------
     # [CLEANSE] df_products
@@ -40,12 +46,17 @@ def cleanse_raw_data(df_products:pd.DataFrame, df_sales:pd.DataFrame)-> pd.DataF
 
     # ----------------------------------------------------
     # [MERGE] LEFT JOIN df_sales and df_products  
-    # ----------------------------------------------------
+    # Check if merge worked expectedly, many_to_one or one_to_one  
+    # 
+    # Implements process for the product_id 999, it'll be replaced to 'Unknown'.  
+    # ----------------------------------------------------    
     tmp_sales = df_sales.copy()
-    merged_df = pd.merge(tmp_sales, tmp_products, on = 'product_id', how = 'left')
+    merged_df = pd.merge(tmp_sales, tmp_products, on = 'product_id', how = 'left', validate="m:1")
+    merged_df[['category']] = merged_df[['category']].fillna('\"Unknown\"')
 
     # ----------------------------------------------------
-    # [MERGE] LEFT JOIN df_sales and df_products  
+    # [ASSERTION] RAISE ASSERTION ERROR
+    # df_sales's row number should not be changed  
     # ----------------------------------------------------
     assert len(merged_df) == len(df_sales), f"Error : df_sales's row number is changed by merge. \n merged_row : {len(merged_df)} \n df_sales_row : {len(df_sales)}."
     return merged_df
